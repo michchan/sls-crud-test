1c371a30f4932232fce68ac469b69b8b6b910523
Michael Chan <michchandev@gmail.com>
2020-03-15 01:37:41 +0800

    Add createPost handler

--- handler.ts
+++ handler.ts
@@ -1,12 +1,46 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
+import { APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
 import 'source-map-support/register';
+import { DynamoDB, AWSError } from 'aws-sdk'
+import uuid from 'uuid/v4'
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
+const db = new DynamoDB.DocumentClient({ apiVersion: '2012-08-10' })
+
+const postsTable = process.env.POSTS_TABLE
+
+/**
+ * Create response
+ */
+function response <M = unknown> (statusCode: number, message: M): APIGatewayProxyResult {
   return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
-  };
+    statusCode,
+    body: JSON.stringify(message)
+  }
+}
+
+/**
+ * Create a new post
+ */
+export const createPost: APIGatewayProxyHandler = async (event) => {
+  // Parse request body
+  const reqBody = JSON.parse(event.body)
+  // Create a new post data
+  const post = {
+    id: uuid(),
+    createdAt: new Date().toISOString(),
+    userId: 1,
+    title: reqBody.title,
+    body: reqBody.body,
+  }
+  // Process
+  try {
+    // Insert into db
+    await db.put({
+      TableName: postsTable,
+      Item: post,
+    })
+    // Response successfully
+    return response(201, post)
+  } catch (error) {
+    return response((error as AWSError).statusCode, error)
+  }
 }
